15afdfd589d823d11d54dd13784698db017c63ed
Stanislav Rogovskyi <stanislav_rogovskyi@epam.com>
2016-08-11 14:06:50 +0300

    asdsadsaddas

--- src/main/java/hello/Handler.java
+++ src/main/java/hello/Handler.java
@@ -12,6 +12,7 @@ public class Handler implements RequestHandler<Request, Response> {
 	public Response handleRequest(Request input, Context context) {
         LambdaLogger logger = context.getLogger();
         logger.log("*** Request handling function body entered. " + input.toString());
+				logger.log("Added new kinds of functionality here");
 		return new Response("Go Serverless v1.0! Your function executed successfully!", input);
 	}
 
